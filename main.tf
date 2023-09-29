3474dbacab0ace8d4f3e5b61344cec2fb44cedd6
Frank Dufort <dufort112@gmail.com>
2023-09-29 00:53:13 +0000

    # 21 change structure of root module and tagging to bucket

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
- cloud {
-    organization = "TerraformBootcamp2023_FD"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   length           = 16
   special          = false
@@ -35,9 +7,10 @@ resource "random_string" "bucket_name" {
 
 resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
-}
+ 
 
