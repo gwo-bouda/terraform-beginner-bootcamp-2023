ee3015b6a0e9b9616f3f4626b970bc3c5c492ea8
Frank Dufort <dufort112@gmail.com>
2023-10-13 23:54:00 +0000

    #43 finish implement of tf provider

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ terraform {
  }
 
 provider "terratowns" {
-  endpoint = "localhost:4567"
+  endpoint = "http://localhost:4567/api"
   user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
   token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 
@@ -40,5 +40,14 @@ provider "terratowns" {
 #  content_version = var.content_version
 #}
 
- 
+ resource "terratowns_home" "home" {
+  name = "Void Bastards"
+  description = <<DESCRIPTION
+Play Void Bastards, it's fun.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = ".cloudfront.net"
+  town = "gamers-grotto"
+  content_version = 1
+}
 
